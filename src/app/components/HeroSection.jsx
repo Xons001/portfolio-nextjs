fe2228156f61f71221abdad22530d1192691f8f1
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-09-21 16:28:40 +0200

    hero section done

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import Image from "next/image";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm
+            </span>
+            <div className="mt-4">
+              <TypeAnimation
+                sequence={[
+                  "Sean",
+                  1000,
+                  "Web Developer",
+                  1000,
+                  "Full Stack Developer",
+                  1000,
+                  "UI/UX Designer",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </div>
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
+            Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+            archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
+            de las industrias desde el año 1500
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              src="/images/fotoSeanCV.jpg"
+              alt="sean image"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
