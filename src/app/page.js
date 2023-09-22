84a5dfbf0f1eb801856cafcfcbdb428aa47e2694
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-09-22 09:42:15 +0200

    NavBar is finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
       <div className="container mx-auto px-14 py-4">
+        <Navbar />
         <HeroSection />
       </div>
     </main>
